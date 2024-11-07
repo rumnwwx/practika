d75422ecbf3d328a93f5376ac021092b10ceb029
rumnwwx <elchaninapolina@gmail.com>
2024-11-07 13:13:43 +0700

    displaying lists and detailed information

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+from django.http import Http404
+
 
 def index(request):
     """
@@ -20,4 +23,31 @@ def index(request):
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
 
-# Create your views here.
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    def book_detail_view(request, pk):
+        try:
+            book_id = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404("Book does not exist")
+
+        # book_id=get_object_or_404(Book, pk=pk)
+
+        return render(
+            request,
+            'catalog/book_detail.html',
+            context={'book': book_id, }
+        )
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
